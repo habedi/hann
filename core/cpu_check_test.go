e64308471f9ad85a942cbe3d3f2f84ac96953313
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-08-20 22:34:20 +0200

    Refactor CPU and logging tests to improve structure and subprocess handling

--- core/cpu_check_test.go
+++ core/cpu_check_test.go
@@ -6,28 +6,21 @@ import (
 	"golang.org/x/sys/cpu"
 )
 
-func initCPUCheck() {
-	// Add any necessary initialization code here
-}
+func TestSupportedCPUFeature(t *testing.T) {
+	// This test checks if the supportedCPUFeature variable is set correctly
+	// based on the CPU capabilities of the machine running the test.
 
-func TestCPUHasAVX(t *testing.T) {
-	if !cpu.X86.HasAVX {
-		defer func() {
-			if r := recover(); r == nil {
-				t.Errorf("Expected panic when CPU does not support AVX")
-			}
-		}()
-		initCPUCheck()
-	}
-}
-
-func TestCPUHasAVXSupport(t *testing.T) {
-	if cpu.X86.HasAVX {
-		defer func() {
-			if r := recover(); r != nil {
-				t.Errorf("Did not expect panic when CPU supports AVX")
-			}
-		}()
-		initCPUCheck()
+	if cpu.X86.HasAVX2 {
+		if supportedCPUFeature != AVX2 {
+			t.Errorf("CPU has AVX2, but supported feature is %v", supportedCPUFeature)
+		}
+	} else if cpu.X86.HasAVX {
+		if supportedCPUFeature != AVX {
+			t.Errorf("CPU has AVX, but supported feature is %v", supportedCPUFeature)
+		}
+	} else {
+		if supportedCPUFeature != Fallback {
+			t.Errorf("CPU has no AVX/AVX2, but supported feature is %v", supportedCPUFeature)
+		}
 	}
 }
